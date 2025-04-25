c978e5d964adf54f141977f00e4f1cbdbfddbcc2
jplazafa <104332639+jplazafa@users.noreply.github.com>
2025-04-25 19:20:34 -0300

    Update Aluno.java

--- src/main/java/com/Aula/Prova1/model/Aluno.java
+++ src/main/java/com/Aula/Prova1/model/Aluno.java
@@ -14,8 +14,9 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
+    private String email;
+    private LocalDate datanascimento;
 
 @ManyToOne
 @JoinColumn(name = "curso_id")
